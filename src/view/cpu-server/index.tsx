dc51ef99f6959b44b71d92b4b4d5af3ecc8002b5
Pedro Ecsedei <pedro.martins55757@gmail.com>
2025-04-14 21:11:03 -0300

    chore: adjusting cpu-server view string

--- src/view/cpu-server/index.tsx
+++ src/view/cpu-server/index.tsx
@@ -6,7 +6,7 @@ export const ServerPage = () => {
     <section className="mt-16">
       <div className="container flex flex-col items-center justify-center h-full min-h-[calc(100vh-64px)]">
         <h1 className="text-center text-white text-4xl font-bold">
-          Processadores de Desktop
+          Processadores de Servidores
         </h1>
 
         <CoreCarousel items={serverCpuContent} />
